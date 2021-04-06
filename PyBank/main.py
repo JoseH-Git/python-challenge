129d94aa4b21c930a6ab1c0ad5eb40cf0c9b9e24
JoseH-Git <joseh.sierra@outlook.com>
2021-04-06 17:09:48 -0500

    First_commit

---
+++ PyBank/main.py
@@ -0,0 +1,50 @@
+# Import Modules
+
+import os
+import csv
+import sys
+
+# Create Lists to determine month and profit
+
+total_month = []
+total_profit = []
+
+# Set path for file
+csvpath = os.path.join("..", "Resources", "budget_data.csv")
+
+# Open the CSV
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    csv_header = next(csvreader)
+    #print(f"CSV Header: {csv_header}")
+    
+    # Loop through lookin
+    for row in csvreader:
+        total_month.append(row[0])
+        total_profit.append(int(row[1]))
+        total_months = len(total_month)
+        total_month_amount = sum(total_profit)
+        average = float(total_month_amount / total_months)
+    
+    print("Financial Analysis")
+    print("----------------------------")
+    print("Total Months: " + str(len(total_month)))
+    print("Total: $" + str(average))
+    print("Average Change: $" + str(sum(total_profit)))
+    print("Greatest Increase in Profits: " + (total_month[total_profit.index(max(total_profit))]) + " ($" +str(max(total_profit))+")")
+    print("Greatest Decrease in Profits: " + (total_month[total_profit.index(min(total_profit))]) + " ($" +str(min(total_profit))+")")
+
+    #Redirect Print to .txt
+
+    sys.stdout=open("main_output.txt","w")
+    print("Financial Analysis")
+    print("----------------------------")
+    print("Total Months: " + str(len(total_month)))
+    print("Total: $" + str(average))
+    print("Average Change: $" + str(sum(total_profit)))
+    print("Greatest Increase in Profits: " + (total_month[total_profit.index(max(total_profit))]) + " ($" +str(max(total_profit))+")")
+    print("Greatest Decrease in Profits: " + (total_month[total_profit.index(min(total_profit))]) + " ($" +str(min(total_profit))+")")
+    sys.stdout.close()
+
+    
---
+++ PyPoll/main.py
@@ -0,0 +1,66 @@
+# Import Modules
+
+import os
+import csv
+import sys
+
+# Create Lists to determine total voters and candidates
+
+total_voters = []
+candidates = []
+
+# Set path for file
+csvpath = os.path.join("..", "Resources", "election_data.csv")
+
+# Open the CSV
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    csv_header = next(csvreader)
+    #print(f"CSV Header: {csv_header}")
+    
+    # Loop through lookin
+    #print(f"CSV Header: {csv_header}")
+    
+    # Loop through CSV file to fill Lists:
+    for row in csvreader:
+        total_voters.append(row[0])
+        candidates.append(row[2])
+
+    # Calculate candidates and total votes:
+
+    new_candidates = list(set(candidates))
+    total_candidates = len(new_candidates)
+    total_votes = []
+    percentage = []
+    candidates_dic = {"names": new_candidates, "votes": total_votes, "percentage": percentage}
+
+    print("Election Results")
+    print("-------------------------")
+    print("Total Voters: " + str(len(total_voters)))
+    print("-------------------------")
+    for i in range(total_candidates):
+        candidates_dic ["names"] = new_candidates[i]
+        candidates_dic ["votes"] = candidates.count(new_candidates[i])
+        candidates_dic ["percentage"] = str(round(candidates_dic ["votes"]*100/(len(total_voters)),2))
+        print(f'{candidates_dic["names"]}: {candidates_dic["percentage"]}% ({candidates_dic["votes"]})')
+    print("-------------------------")
+    print("Winner:"+ candidates_dic["names"] + " " + max(candidates_dic["percentage"])) 
+    print("-------------------------")
+
+    #Redirect Print to .txt
+
+    sys.stdout=open("main_output.txt","w")
+    print("Election Results")
+    print("-------------------------")
+    print("Total Voters: " + str(len(total_voters)))
+    print("-------------------------")
+    for i in range(total_candidates):
+        candidates_dic ["names"]= new_candidates[i]
+        candidates_dic ["votes"]= candidates.count(new_candidates[i])
+        candidates_dic ["percentage"] = str(round(candidates_dic ["votes"]*100/(len(total_voters)),2))
+        print(f'{candidates_dic["names"]}: {candidates_dic["percentage"]}% ({candidates_dic["votes"]})')
+    print("-------------------------")
+    print("Winner:"+ candidates_dic["names"] + max(candidates_dic["percentage"])) 
+    print("-------------------------")
+    sys.stdout.close()
